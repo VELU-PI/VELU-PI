935ef9b786ae637b7bd2ea4fff5e58ec46e62f7a
VELU-PI <168462972+VELU-PI@users.noreply.github.com>
2024-05-05 21:43:13 +0200

    Delete mysql_query_image_video_bme680.py

--- mysql_query_image_video_bme680.py
+++
@@ -1,9 +0,0 @@
-CREATE TABLE image_video_bme680 (
-  `date` DATETIME DEFAULT NULL,
-  `host` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `image` MEDIUMBLOB DEFAULT NULL,
-  `video` LONGBLOB DEFAULT NULL,
-  `temperature` float DEFAULT NULL,
-  `pressure` float DEFAULT NULL,
-  `humidity` float DEFAULT NULL
-);
